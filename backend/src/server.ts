e4d3c0f4062328b995501b1d200003c3d5cc254b
asmaa430 <asmaamelwaer@gmail.com>
2025-12-20 01:01:40 +0200

    update server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,4 +13,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/memories", memoryItemRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+  res.send(`API is running on port ${port}`);
+});
