a26127a16a2d8f8511197d00d5fbec893de1dda0
Mohammed Hossam <mohammedhossam343@gmail.com>
2026-01-11 08:05:01 +0200

    Added the Routes for the face

---
+++ backend/src/routes/faceRoutes.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import multer from "multer";
+import {
+    registerPatientFace,
+    identifyPatientByFace
+} from "../controllers/faceController";
+import { protect } from "../middlewares/authMiddleware";
+
+const router = express.Router();
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: { fileSize: 5 * 1024 * 1024 }
+});
+// -----------------------------
+// Register Patient Face
+// -----------------------------
+router.post(
+    "/patient/register-face",
+    protect,
+    upload.single("image"),
+    registerPatientFace
+);
+// -----------------------------
+// Identify Patient Face
+// -----------------------------
+router.post(
+    "/patient/identify-face",
+    protect,
+    upload.single("image"),
+    identifyPatientByFace
+);
+export default router;
